f61fe615eb8a5fe3d5af6fcb7e8467942748edec
yzt <2543761065@qq.com>
2019-11-09 00:56:27 +0800

    finish.

---
+++ ParticleSystem/Assets/Halo.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Halo : MonoBehaviour {
+
+    public class Position
+    {
+        public float radius;
+        public float angle1;
+        public float angle2;
+        public Position(float r, float a,float b)
+        {
+            radius = r;
+            angle1 = a;
+            angle2 = b;
+        }
+    }
+
+    private ParticleSystem sys;              
+    private ParticleSystem.Particle[] particals; 
+    private Position[] positions;             
+
+    public int num = 10000;       
+    public float size = 0.3f;  
+    public bool vary=true;
+
+    void Start () {
+        sys = this.GetComponent<ParticleSystem>();
+        particals = new ParticleSystem.Particle[num];
+        positions = new Position[num];
+
+        var main = sys.main;
+        main.startSpeed = 0;
+        main.startSize = size;
+        main.loop = false;
+        main.maxParticles = num;
+        sys.Emit(num);
+        sys.GetParticles(particals);
+
+        for (int i = 0; i < num; i++)
+        {
+            float radius=3f;
+            float angle1 = Random.Range(0f, 360f);
+            float radian1 = angle1 * 180 / Mathf.PI;
+            float angle2 = Random.Range(0f, 360f);
+            float radian2 = angle2 * 180 / Mathf.PI;
+            
+            positions[i] = new Position(radius, angle1,angle2);
+            particals[i].position =
+                new Vector3(positions[i].radius * Mathf.Cos(radian2) * Mathf.Cos(radian1),
+                            positions[i].radius * Mathf.Sin(radian2),
+                            positions[i].radius * Mathf.Cos(radian2) * Mathf.Sin(radian1));
+        }
+        sys.SetParticles(particals, particals.Length);
+	}
+	
+	void Update () {
+        if(vary==true)
+        {
+            for(int i=0;i<num;i++)
+            {
+                positions[i].radius-=0.01f;
+            }
+            if(positions[0].radius<=0.5){
+                vary=false;
+            }
+        }
+        else{
+            for(int i=0;i<num;i++)
+            {
+                positions[i].radius+=0.01f;
+            }
+            if(positions[0].radius>=3){
+                vary=true;
+            }
+        }
+
+        for (int i = 0; i < num; i++)
+        {
+            positions[i].angle1 = (positions[i].angle1 - Random.Range(0.5f,3f)) % 360f;
+            positions[i].angle2 = (positions[i].angle2 - Random.Range(0.5f,3f) + 180) % 360f - 180;             
+            float radian1 = positions[i].angle1 / 180 * Mathf.PI;
+            float radian2 = positions[i].angle2 / 180 * Mathf.PI;
+            particals[i].position = new Vector3(positions[i].radius * Mathf.Cos(radian2) * Mathf.Cos(radian1),
+                            positions[i].radius * Mathf.Sin(radian2),
+                            positions[i].radius * Mathf.Cos(radian2) * Mathf.Sin(radian1));
+        }
+        sys.SetParticles(particals, particals.Length);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 3d_8
+
+视频：
+
+https://www.bilibili.com/video/av75012011
+
+本作业参考了网上博客，具体详情见：
+
+https://blog.csdn.net/SYSU_yzt/article/details/102980074
